26ff0516fa4e7fc7854fbb522f6caa943b71aed5
TarCode <tarcode33@gmail.com>
2018-09-21 22:01:51 +0200

    added get request to pokemon api and display results to console

---
+++ hello/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace hello
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create(
+              "https://pokeapi.co/api/v2/pokemon/1");
+            // If required by the server, set the credentials.  
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.  
+            reader.Close();
+            response.Close();
+        }
+    }
+}
